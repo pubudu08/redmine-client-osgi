40c2e70a0c109d8fedcf7c206f4e4401ce3dc69b
Pubudu Dissanayake <pubudu08@gmail.com>
2014-09-15 08:53:59 +0530

    Codereview - Refactored exception handling

--- src/main/java/org/wso2/carbon/utility/redmine/internal/ServiceComponent.java
+++ src/main/java/org/wso2/carbon/utility/redmine/internal/ServiceComponent.java
@@ -5,35 +5,38 @@ import org.apache.commons.logging.LogFactory;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.component.ComponentContext;
-import org.wso2.carbon.utility.projectmanagement.IProjectManagement;
+import org.wso2.carbon.utility.projectmanagement.ProjectManagementArtifact;
 import org.wso2.carbon.utility.redmine.service.PMAdminService;
 
 /**
  * Created by pubudu on 2/3/14.
+ *
  * @scr.component name="org.wso2.carbon.utility.redmine"
  * immediate="true"
  */
 public class ServiceComponent {
 
-    private ServiceRegistration serviceRegistry;
-    private static BundleContext  bundleContext;
-    private static PMAdminService pmAdminService;
-    private static final Log logger = LogFactory.getLog(ServiceComponent.class);
+	private ServiceRegistration serviceRegistry;
+	private static BundleContext bundleContext;
+	private static PMAdminService pmAdminService;
+	private static final Log LOGGER = LogFactory.getLog(ServiceComponent.class);
 
 
-    public void activate(ComponentContext componentContext){
-        logger.info("Project management Service: Redmine bundle is activated");
-        pmAdminService= new PMAdminService();
-        bundleContext = componentContext.getBundleContext();
-        serviceRegistry = bundleContext.registerService(IProjectManagement.class.getName(),pmAdminService, null);
+	public void activate(ComponentContext componentContext) {
+		LOGGER.info("Project management Service: Redmine bundle is activated");
+		pmAdminService = new PMAdminService();
+		bundleContext = componentContext.getBundleContext();
+		serviceRegistry = bundleContext.registerService(
+		  ProjectManagementArtifact.class.getName(),
+		  pmAdminService, null);
 
-    }
+	}
 
-    public void deactivate(ComponentContext componentContext){
-        logger.info("Project management Service: Redmine bundle is deactivated");
-        serviceRegistry.unregister();
-        bundleContext=null;
-        pmAdminService=null;
-    }
+	public void deactivate(ComponentContext componentContext) {
+		LOGGER.info("Project management Service: Redmine bundle is deactivated");
+		serviceRegistry.unregister();
+		bundleContext = null;
+		pmAdminService = null;
+	}
 
 }
--- src/main/java/org/wso2/carbon/utility/redmine/service/PMAdminService.java
+++ src/main/java/org/wso2/carbon/utility/redmine/service/PMAdminService.java
@@ -7,7 +7,8 @@ import com.taskadapter.redmineapi.bean.User;
 import com.taskadapter.redmineapi.bean.Version;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.wso2.carbon.utility.projectmanagement.IProjectManagement;
+import org.wso2.carbon.utility.projectmanagement.ProjectManagementArtifact;
+import org.wso2.carbon.utility.redmine.exception.GenericArtifactException;
 
 import java.util.Date;
 import java.util.List;
@@ -15,138 +16,146 @@ import java.util.List;
 /**
  * PMAdminService is a class which implements IProjectManagement
  * ReadMine API faces against the pulggable PM interface
+ *
  * @author Pubudu Dissanayake : pubudud@wso2.com  on 02/03/2014.
  */
-public class PMAdminService implements IProjectManagement{
+public class PMAdminService implements ProjectManagementArtifact {
 
-    private static final Log logger = LogFactory.getLog(PMAdminService.class);
-    private RedmineManager mgr = null;
+	private static final Log LOGGER = LogFactory.getLog(PMAdminService.class);
+	private RedmineManager redmineManager = null;
 
-    /**
-     * This method will returns artifact type
-     * @return <code>Redmine</code>
-     **/
-    @Override
-    public String getPMSType() {
-        return "Redmine";
-    }
-    /**
-     * This method will create a project which is related to PM
-     * @param pmHost the host name to create a project
-     * @param username username
-     * @param password password
-     * @param projectName  project name
-     * @return  <code>true</code> if project created successfully
-     */
-    @Override
-    public boolean createPMSProject(String redmineHost, String username,String password,  String projectName) {
+	/**
+	 * This method will returns artifact type
+	 *
+	 * @return <code>Redmine</code>
+	 */
+	@Override
+	public String getPMSType() {
+		return "Redmine";
+	}
 
-        mgr=  new RedmineManager(redmineHost,username,password);
-        Project project=null;
-        try {
-            logger.info(mgr.getCurrentUser());
-            project = new Project( );
-            project.setName(projectName);
-            projectName  = projectName.replaceAll(" ", "-");
-            project.setIdentifier(projectName);
-            project.setCreatedOn(new Date());
-            project.setIdentifier(projectName);
-            mgr.createProject(project);
-            return isPMSProjectExist(redmineHost,username,password,projectName);
+	/**
+	 * This method will create a project which is related to PM
+	 *
+	 * @param pmHost      the host name to create a project
+	 * @param username    username
+	 * @param password    password
+	 * @param projectName project name
+	 * @return <code>true</code> if project created successfully
+	 */
+	@Override
+	public void createPMSProject(String redmineHost,String username,
+	                             String password,String projectName)
+	  throws GenericArtifactException,RedmineException {
 
-        } catch (RedmineException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
+		Project project;
+		redmineManager = new RedmineManager(redmineHost, username, password);
+		try {
+			if (LOGGER.isDebugEnabled()) {
+				LOGGER.debug("Redmine account login user : " + redmineManager.getCurrentUser());
+			}
+			for (Project tempProject : redmineManager.getProjects()) {
+				if (projectName.equals(tempProject.getName())) {
+					throw new GenericArtifactException(
+					  "Project is already exists, Please provide" +
+					  "a suitable project name. ", "Project_Already_Exists");
+				}
+			}
+			project = new Project();
+			project.setName(projectName);
+			projectName = projectName.replaceAll(" ", "-");
+			project.setIdentifier(projectName);
+			project.setCreatedOn(new Date());
+			project.setIdentifier(projectName);
+			redmineManager.createProject(project);
 
-    /**
-     * check whether project is exists or not
-     * @param pmHost   Host name of the API
-     * @param username username
-     * @param password password
-     * @param projectName  name of the project to search
-     * @return  <code>true</code> if project already exists
-     */
-    @Override
-    public boolean isPMSProjectExist(String redmineHost, String username,String password, String projectName) {
-        mgr = new RedmineManager(redmineHost,username,password);
-        try {
-            logger.info(mgr.getCurrentUser());
-            List<Project> projects = mgr.getProjects();
-            for(Project project:projects){
-                if (projectName.equals(project.getName())){
-                    return true;
-                }
-            }
-        } catch (RedmineException e) {
-            e.printStackTrace();
-            return false;
-        }
-        return false;
-    }
+			if (LOGGER.isDebugEnabled()) {
+				LOGGER.debug("Redmine project successfully created by : " +
+				             redmineManager.getCurrentUser() + ", timestamp " + new Date().getTime());
+			}
+		} catch (RedmineException e) {
+			LOGGER.error("Unable to connect Redmine servers", e);
+			throw new RedmineException();
+		} finally {
+			if (redmineManager != null) {
+				redmineManager = null;
+			}
+		}
+	}
 
-    /**
-     * get project by its Id
-     * @param redmineHost
-     * @param username username
-     * @param password password
-     * @param projectkey
-     * @return Redmine Project
-     */
-    public Project getProjectById(String redmineHost, String username,String password, String projectkey){
-        mgr= new RedmineManager(redmineHost,username,password);
-        Project project = null;
-        try {
-            project = mgr.getProjectByKey(projectkey);
+	/**
+	 * get project by its Id
+	 *
+	 * @param redmineHost redmine hostname
+	 * @param username    username
+	 * @param password    password
+	 * @param projectKey
+	 * @return Redmine Project
+	 */
+	public Project getProjectById(String redmineHost,String username,
+	                              String password,String projectKey)
+	  throws RedmineException {
+		redmineManager = new RedmineManager(redmineHost, username, password);
+		Project project;
+		try {
+			project = redmineManager.getProjectByKey(projectKey);
+		} catch (RedmineException e) {
+			LOGGER.error("Unable to connect Redmine servers", e);
+			throw new RedmineException();
+		}
+		return project;
+	}
 
-        } catch (RedmineException e) {
-            e.printStackTrace();
-        }
+	/**
+	 * get the current logged user
+	 *
+	 * @param redmineHost redmine hostname
+	 * @param username    username
+	 * @param password    password
+	 * @return User type object
+	 */
+	public User getCurrentUser(String redmineHost,
+	                           String username,
+	                           String password)
+	  throws RedmineException {
+		redmineManager = new RedmineManager(redmineHost, username, password);
+		User user;
+		try {
+			user = redmineManager.getCurrentUser();
+		} catch (RedmineException e) {
+			LOGGER.error("Unable to connect Redmine servers", e);
+			throw new RedmineException();
+		}
+		return user;
+	}
 
-        return project;
-    }
+	/**
+	 * Get list of projects
+	 *
+	 * @param redmineHost redmine hostname
+	 * @param username    username
+	 * @param password    password
+	 * @param projectKey  project key
+	 * @return Array list of projects
+	 */
+	public List<Version> getProjectVersions(String redmineHost,
+	                                        String username,
+	                                        String password,
+	                                        String projectKey)
+	  throws RedmineException {
+		redmineManager = new RedmineManager(redmineHost, username, password);
+		List<Version> projectVersions;
 
-    /**
-     * get the current logged user
-     * @param redmineHost
-     * @param username username
-     * @param password password
-     * @return User type object
-     */
-    public User getCurrentUser(String redmineHost, String username,String password ){
-        mgr = new RedmineManager(redmineHost,username,password);
-        User user = null;
-        try {
-            user = mgr.getCurrentUser();
-        } catch (RedmineException e) {
-            e.printStackTrace();
-        }
-        return user;
+		try {
+			projectVersions = redmineManager.getVersions(Integer.parseInt(projectKey));
+		} catch (RedmineException e) {
+			LOGGER.error("Unable to connect Redmine servers", e);
+			throw new RedmineException();
+		}
+		return projectVersions;
 
-    }
 
-    /**
-     * Get list of projects
-     * @param redmineHost
-     * @param username username
-     * @param password password
-     * @param projectKey
-     * @return Array list of projects
-     */
-    public List<Version> getProjectVersions(String redmineHost, String username,String password, String projectKey){
-        mgr = new RedmineManager(redmineHost,username,password);
-        List<Version> projectVersions = null;
-
-        try {
-            projectVersions = mgr.getVersions(Integer.parseInt(projectKey));
-        } catch (RedmineException e) {
-            e.printStackTrace();
-        }
-        return projectVersions;
-
-    }
-
+	}
 
 
 }
